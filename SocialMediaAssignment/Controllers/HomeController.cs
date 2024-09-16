510cd88f1e6b97e731a7c0da302f9f9a123c932c
arishavellekarl.villanueva <ArishavelleKarl.Villanueva@blackfort.ph>
2024-09-16 16:08:12 +0800

    Add project files.

---
+++ SocialMediaAssignment/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using SocialMediaAssignment.Models;
+
+namespace SocialMediaAssignment.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly IConfiguration _configuration;
+    public HomeController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    [Route("/")]
+    public IActionResult Index()
+    {
+        IConfigurationSection socials = _configuration.GetSection("SocialMediaLinks");
+        SocialMediaLinksOptions socialMedia = new SocialMediaLinksOptions()
+        {
+            Facebook = socials["Facebook"],
+            Instagram = socials["Instagram"],
+            Twitter = socials["Twitter"],
+            Youtube = socials["Youtube"]
+        };
+        return View(socialMedia);
+    }
+}
